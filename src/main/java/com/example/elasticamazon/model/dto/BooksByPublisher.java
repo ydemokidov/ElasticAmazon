b53cf21534d75f23bb0972fe547df05016c96432
ydemokidov <63907380+ydemokidov@users.noreply.github.com>
2023-05-02 17:05:41 +0300

    name fixed

--- src/main/java/com/example/elasticamazon/model/dto/BooksByPublisher.java
+++ src/main/java/com/example/elasticamazon/model/dto/BooksByPublisher.java
@@ -11,7 +11,7 @@ import java.util.Map;
 @Getter
 @Setter
 @NoArgsConstructor
-public class BookSByPublisher {
+public class BooksByPublisher {
 
     private Map<String, Map<Double, List<String>>> booksByPublishers = new HashMap<>();
 
